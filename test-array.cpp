4946de6341205b86d59ba4f02b68b6b9ea6dbca8
mthek10 <33663144+mthek10@users.noreply.github.com>
2020-01-29 22:30:13 -0500

    adds tests for hash, equals, to string

--- test-array.cpp
+++ test-array.cpp
@@ -18,10 +18,10 @@ void test1() {
   } else {
     std::cout << "Fail 1" << std::endl;
   }
-    delete a;
-    delete s1;
-    delete s2;
-    delete s3;
+  delete a;
+  delete s1;
+  delete s2;
+  delete s3;
 }
 
 void test2() {
@@ -35,9 +35,9 @@ void test2() {
   } else {
     std::cout << "Fail 2" << std::endl;
   }
-    delete a;
-    delete s1;
-    delete s2;
+  delete a;
+  delete s1;
+  delete s2;
 }
 
 void test3() {
@@ -47,28 +47,131 @@ void test3() {
   } else {
     std::cout << "Fail 3" << std::endl;
   }
-    delete a;
+  delete a;
 }
 
 
 void test4() {
   Array* a = new Array(10);
-  String* c = new String();
-  String* d = new String();
+  String* c = new String("world");
+  String* d = new String("hello");
   a->set(0, c);
   a->set(1, d);
   if ((a->get(0)->equals(c) && (a->get(1)->equals(d)) && a->length() == 2)) {
     std::cout << "Pass 4" << std::endl;
-    delete a;
-    delete c;
-    delete d;
   } else {
-    std::cout << "Fail 4" << std::endl;
-    delete a;
-    delete c;
-    delete d;
-    exit(0);
+      std::cout << "Fail 4" << std::endl;
   }
+  delete a;
+  delete c;
+  delete d;
+}
+
+// tests push_back functionality
+void test5() {
+  Array* a = new Array(10);
+  String* b = new String("foo");
+  String* c = new String("baz");
+  String* d = new String("bar");
+  a->push_back(b);
+  a->push_back(c);
+  a->push_back(d);
+
+  if ((a->size_ == 3) && (a->get(0)->equals(b))
+    && (a->get(1)->equals(c)) && (a->get(2)->equals(d))) {
+      std::cout << "Pass 5" << std::endl;
+  } else {
+      std::cout << "Fail 5" << std::endl;
+  }
+  delete a;
+  delete b;
+  delete c;
+  delete d;
+}
+
+// tests to_string functionality
+void test6() {
+  Array* a = new Array(10);
+  String* b = new String("foo");
+  String* c = new String("baz");
+  String* d = new String("bar");
+  a->push_back(b);
+  a->push_back(c);
+  a->push_back(d);
+  String* e = new String("{'foo', 'baz', 'bar'}");
+  String* f = new String(a->to_string());
+  Array* a2 = new Array(10);
+  String* g = new String("{}");
+  String* h = a2->to_string();
+
+  if (e->equals(f) && g->equals(h)) {
+    std::cout << "Pass 6" << std::endl;
+  } else {
+    std::cout << "Fail 6" << std::endl;
+  }
+  delete a;
+  delete b;
+  delete c;
+  delete d;
+  delete e;
+  delete f;
+  delete a2;
+  delete g;
+  delete h;
+}
+
+// tests equals functionality
+void test7() {
+  Array* a1 = new Array(10);
+  String* b = new String("foo");
+  String* c = new String("baz");
+  String* d = new String("bar");
+  a1->push_back(b);
+  a1->push_back(c);
+  a1->push_back(d);
+
+  Array* a2 = new Array(10);
+  String* b2 = new String("foo");
+  String* c2 = new String("baz");
+  String* d2 = new String("bar");
+  a2->push_back(b2);
+  a2->push_back(c2);
+  a2->push_back(d2);
+
+  Array* a3 = new Array(10);
+
+  if (a1->equals(a2) && !a1->equals(a3)) {
+    std::cout << "Pass 7" << std::endl;
+  } else {
+    std::cout << "Fail 7" << std::endl;
+  }
+  delete a1;
+  delete b;
+  delete c;
+  delete d;
+  delete a2;
+  delete b2;
+  delete c2;
+  delete d2;
+}
+
+// tests hash method
+void test8() {
+  Array* a1 = new Array(10);
+  Array* a2 = new Array(10);
+  String* s1 = new String("hi");
+  String* s2 = new String("hi");
+  a1->push_back(s1);
+  a2->push_back(s2);
+  if (a1->hash() == a2->hash()) {
+    std::cout << "Pass 7" << std::endl;
+  } else {
+    std::cout << "Fail 7" << std::endl;
+  }
+  delete a1;
+  delete a2;
+  delete s1;
+  delete s2;
 }
 
 int main() {
@@ -76,6 +179,9 @@ int main() {
   test2();
   test3();
   test4();
+  test5();
+  test6();
+  test7();
+  test8();
   return 0;
 }
-
